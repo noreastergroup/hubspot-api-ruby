ed629a6991e71083deeb6c4c74672a6975f4a5a0
John Nagro <john@noreastergroup.com>
2024-10-04 13:02:22 -0400

    an upsert batch method

--- lib/hubspot/codegen/crm/contacts/api/batch_api.rb
+++ lib/hubspot/codegen/crm/contacts/api/batch_api.rb
@@ -226,7 +226,7 @@ module Hubspot
         # @param batch_input_simple_public_object_batch_input [BatchInputSimplePublicObjectBatchInput] 
         # @param [Hash] opts the optional parameters
         # @return [BatchResponseSimplePublicObject]
-        def update(batch_input_simple_public_object_batch_input, opts = {})
+        def update(batch_inpuct_simple_public_object_batch_input, opts = {})
           data, _status_code, _headers = update_with_http_info(batch_input_simple_public_object_batch_input, opts)
           data
         end
@@ -287,6 +287,74 @@ module Hubspot
           end
           return data, status_code, headers
         end
+
+        # Upsert a batch of contacts
+        # @param batch_input_simple_public_object_batch_input [BatchInputSimplePublicObjectBatchInput] 
+        # @param [Hash] opts the optional parameters
+        # @return [BatchResponseSimplePublicObject]
+        def upsert(batch_input_simple_public_object_batch_input, opts = {})
+          data, _status_code, _headers = upsert_with_http_info(batch_input_simple_public_object_batch_input, opts)
+          data
+        end
+
+        # Upsert a batch of contacts
+        # @param batch_input_simple_public_object_batch_input [BatchInputSimplePublicObjectBatchInput] 
+        # @param [Hash] opts the optional parameters
+        # @return [Array<(BatchResponseSimplePublicObject, Integer, Hash)>] BatchResponseSimplePublicObject data, response status code and response headers
+        def upsert_with_http_info(batch_input_simple_public_object_batch_input, opts = {})
+          if @api_client.config.debugging
+            @api_client.config.logger.debug 'Calling API: BatchApi.upsert ...'
+          end
+          # verify the required parameter 'batch_input_simple_public_object_batch_input' is set
+          if @api_client.config.client_side_validation && batch_input_simple_public_object_batch_input.nil?
+            fail ArgumentError, "Missing the required parameter 'batch_input_simple_public_object_batch_input' when calling BatchApi.upsert"
+          end
+          # resource path
+          local_var_path = '/crm/v3/objects/contacts/batch/upsert'
+
+          # query parameters
+          query_params = opts[:query_params] || {}
+
+          # header parameters
+          header_params = opts[:header_params] || {}
+          # HTTP header 'Accept' (if needed)
+          header_params['Accept'] = @api_client.select_header_accept(['application/json', '*/*'])
+          # HTTP header 'Content-Type'
+          content_type = @api_client.select_header_content_type(['application/json'])
+          if !content_type.nil?
+              header_params['Content-Type'] = content_type
+          end
+
+          # form parameters
+          form_params = opts[:form_params] || {}
+
+          # http body (model)
+          post_body = opts[:debug_body] || @api_client.object_to_http_body(batch_input_simple_public_object_batch_input)
+
+          # return_type
+          return_type = opts[:debug_return_type] || 'BatchResponseSimplePublicObject'
+
+          # auth_names
+          auth_names = opts[:debug_auth_names] || ['oauth2']
+
+          new_options = opts.merge(
+            :operation => :"BatchApi.upsert",
+            :header_params => header_params,
+            :query_params => query_params,
+            :form_params => form_params,
+            :body => post_body,
+            :auth_names => auth_names,
+            :return_type => return_type
+          )
+
+          data, status_code, headers = @api_client.call_api(:POST, local_var_path, new_options)
+          if @api_client.config.debugging
+            @api_client.config.logger.debug "API called: BatchApi#upsert\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+          end
+          return data, status_code, headers
+        end
+
+
       end
     end
   end
